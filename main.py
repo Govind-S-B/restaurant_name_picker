2e0baa158931f999bde111f6e1c76473c41b962f
Govind-S-B <b.s.dnivog@gmail.com>
2023-11-05 10:46:28 +0530

    added a chain thats reusable

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from langchain.llms import Ollama
 from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
 
 llm = Ollama(model="zephyr",
              temperature="1")
 
 cusine_prompt_template = PromptTemplate(
     input_variables=["cuisine "],
-    template="""I want to open a resturant for {cuisine}. Suggest a name for this restaurant.
+    template="""I want to open a resturant for {cuisine} food. Suggest a fancy name for this restaurant.
     The response should be just a single name and nothing else"""
 )
 
-name = llm("I want to open a resturant for French cusine mixed with indian cusine , a fusion style . Suggest a fancy name for this restaurant . The response should be just a single name and nothing else")
-print(name)
+chain = LLMChain(llm=llm, prompt=cusine_prompt_template)
+print(chain.run("an indian and french fusion cuisine"))
