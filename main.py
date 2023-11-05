de5d8875ebba7e2d7e6f586e0ddde292c54f58a7
Govind-S-B <b.s.dnivog@gmail.com>
2023-11-06 01:40:29 +0530

    sequential chain

--- main.py
+++ main.py
@@ -1,28 +1,31 @@
 from langchain.llms import Ollama
 from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain, SimpleSequentialChain
+from langchain.chains import LLMChain, SequentialChain
 
 llm = Ollama(model="zephyr",
-             temperature="1")
+             temperature="0.6")
 
 cusine_prompt_template = PromptTemplate(
     input_variables=["cuisine "],
-    template="""I want to open a restaurant for {cuisine} food. Suggest a fancy name for this restaurant.
-    The response should be just a single name and nothing else"""
+    template="""I want to open a restaurant for {cuisine} food. Suggest a fancy name for this restaurant."""
 )
 
-restaurant_name_chain = LLMChain(llm=llm, prompt=cusine_prompt_template)
+restaurant_name_chain = LLMChain(
+    llm=llm, prompt=cusine_prompt_template, output_key="restaurant_name")
 
 menu_prompt_template = PromptTemplate(
     input_variables=["restaurant_name"],
-    template="""Give a 10 item menu for a restaurant named {restaurant_name}.
-    The response should be a list of comma seperated values in the format : 
-    [item1,item2,item3]"""
+    template="""Give a 10 item menu for a restaurant named {restaurant_name}"""
 )
 
-restaurant_menu_chain = LLMChain(llm=llm, prompt=menu_prompt_template)
+restaurant_menu_chain = LLMChain(
+    llm=llm, prompt=menu_prompt_template, output_key="menu_items")
 
-chain = SimpleSequentialChain(
-    chains=[restaurant_name_chain, restaurant_menu_chain])
+chain = SequentialChain(
+    chains=[restaurant_name_chain, restaurant_menu_chain],
+    input_variables=["cuisine"],
+    output_variables=["restaurant_name", "menu_items"])
 
-print(chain.run("an indian and french fusion cuisine"))
+print(chain({
+    "cuisine": "french indian fushion"
+}))
