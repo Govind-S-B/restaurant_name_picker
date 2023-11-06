f478572dca050ecd2210db603c0dbbd6aa016f7b
Govind-S-B <b.s.dnivog@gmail.com>
2023-11-07 00:25:04 +0530

    fixed the summarization pipeline , can finetune with repetion penalty fine tuning and persona prompting

--- langchain_helper.py
+++ langchain_helper.py
@@ -6,8 +6,8 @@ from langchain.chains import LLMChain, SequentialChain
 llm = Ollama(model="zephyr",
              temperature="0.6")
 
-llm_mistral = Ollama(model="mistral",
-                     temperature="0.2")
+llm_mistral = Ollama(model="dolphin2.2-mistral",
+                     temperature="0")
 
 
 def get_restaurant_name_and_menu(cuisine):
@@ -19,7 +19,9 @@ def get_restaurant_name_and_menu(cuisine):
 
     cusine_prompt_template = PromptTemplate(
         input_variables=["cuisine "],
-        template="""I want to open a restaurant for {cuisine} food. Suggest a fancy name for this restaurant."""
+        template="""You are a AI model specialized in creative thinking.
+        
+        I want to open a restaurant for {cuisine} food. Suggest a fancy name for this restaurant."""
     )
 
     restaurant_name_chain = LLMChain(
@@ -29,11 +31,13 @@ def get_restaurant_name_and_menu(cuisine):
 
     name_clean_up_template = PromptTemplate(
         input_variables=["restaurant_name"],
-        template="""from the following text, extract a name for an restaurant, it should just be a single name and nothing more.
-        When given multiple options choose the best one that feels catchy and fancy but only one should be given.
+        template="""You are a text summarization and formatting model which specializes in creating structured data.
+        
+        From the following text, extract a single name for a restaurant, When given multiple options choose the best one that feels catchy and fancy while keeping the meaning.
+        You are to return only a single name , strictly follow that format. Dont return descriptions or sentences.
 
-
-    {restaurant_name}
+        Beginnging of Text :
+        {restaurant_name}
     """
     )
 
@@ -44,7 +48,9 @@ def get_restaurant_name_and_menu(cuisine):
 
     menu_prompt_template = PromptTemplate(
         input_variables=["cleaned_name"],
-        template="""Give a 10 item menu for a restaurant named {cleaned_name}"""
+        template="""You are a AI model specialized in creative thinking.
+        
+        Give a 10 item menu for a restaurant named {cleaned_name}"""
     )
 
     restaurant_menu_chain = LLMChain(
@@ -54,11 +60,15 @@ def get_restaurant_name_and_menu(cuisine):
 
     menu_clean_up_template = PromptTemplate(
         input_variables=["menu_items"],
-        template="""from the following text, extract a list of comma seperated values for the menu items for an restaurant.
-        it should strictly be in the format specified. Ignore any category titles ,only extract the menu items in the list
+        template="""You are a text summarization and formatting model which specializes in creating structured data.
+        
+        From the following text, extract a list of comma seperated values for the menu items for an restaurant.
+        It should strictly be in the format specified. Remove any category titles or newline characters or other artifacts,
+        only extract the menu items from the text into the list.
 
-    {menu_items}
-    """
+        Beginnging of Text :
+        {menu_items}
+        """
     )
 
     restaurant_menu_clean_up_chain = LLMChain(
@@ -83,5 +93,6 @@ def get_restaurant_name_and_menu(cuisine):
 
 
 if __name__ == "__main__":
-    data = get_restaurant_name_and_menu("indo french fusion")
-    print(json.dumps(data, indent=4))
+    for i in range(20):
+        data = get_restaurant_name_and_menu("mexican italian fusion")
+        print(json.dumps(data, indent=4))
