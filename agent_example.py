f79673a55f6b26518ae60eb842c26a3458f2c575
Govind-S-B <b.s.dnivog@gmail.com>
2023-11-07 20:14:27 +0530

    swapped wikipedia for duckduckgo search

--- agent_example.py
+++ agent_example.py
@@ -1,10 +1,18 @@
 from langchain.agents import AgentType, initialize_agent, load_tools
 from langchain.llms import Ollama
 
+# from langchain.tools import Tool, DuckDuckGoSearchRun, ArxivQueryRun, WikipediaQueryRun
+# from langchain.utilities import WikipediaAPIWrapper
+# from langchain.agents import initialize_agent
+# from langchain.agents import AgentType
+# from langchain.chat_models import ChatOpenAI
+# from langchain.chains import LLMChain
+# from langchain.prompts import PromptTemplate
+
 llm = Ollama(model="dolphin2.2-mistral",
              temperature="0.2")
 
-tools = load_tools(["wikipedia", "llm-math"], llm=llm)
+tools = load_tools(["llm-math", "ddg-search"], llm=llm)
 
 agent = initialize_agent(
     tools, llm, AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True
