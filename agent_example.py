2453dc0977cb18cd0d877a5bf02bd9a83a5f1ddb
Govind-S-B <b.s.dnivog@gmail.com>
2023-11-07 19:27:06 +0530

    a poorly working agent test

---
+++ agent_example.py
@@ -0,0 +1,13 @@
+from langchain.agents import AgentType, initialize_agent, load_tools
+from langchain.llms import Ollama
+
+llm = Ollama(model="dolphin2.2-mistral",
+             temperature="0.2")
+
+tools = load_tools(["wikipedia", "llm-math"], llm=llm)
+
+agent = initialize_agent(
+    tools, llm, AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True
+)
+
+print(agent.run("how old is elon musk going to be at the year 2030"))
