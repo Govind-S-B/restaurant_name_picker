c2405897d8684e139f4bb002d8a7de466ec19d37
Govind-S-B <b.s.dnivog@gmail.com>
2023-11-09 19:39:03 +0530

    window memory

--- memory_example.py
+++ memory_example.py
@@ -1,11 +1,13 @@
 from langchain.llms import Ollama
 from langchain.prompts import PromptTemplate
-from langchain.memory import ConversationBufferMemory
+from langchain.memory import ConversationBufferWindowMemory
 from langchain.chains import ConversationChain
 
 llm = Ollama(model="dolphin2.2-mistral",
              temperature="0.6",
              system="You are a very helpful AI assistant")
+
+memory = ConversationBufferWindowMemory(k=10)
 
 chain = ConversationChain(llm=llm)
 
